aaf54b25d9d693acefcf05a1c7f7effed32c97d0
3 <www.reinhart@gmail.com>
2025-09-22 09:03:46 +0800

    initial feedback page

--- app/(application)/feedback/[eventId].tsx
+++ app/(application)/feedback/[eventId].tsx
@@ -1,6 +1,7 @@
 import useGesture from "@/hooks/useGesture";
 import { Box } from "@/components/ui/box";
 import { Text } from "@/components/ui/text";
+import { Button, ButtonText } from "@/components/ui/button";
 
 function Feedback() {
   const { SwipeableComponent } = useGesture();
@@ -8,6 +9,13 @@ function Feedback() {
     <SwipeableComponent className="flex flex-1">
       <Box className="flex flex-1">
         <Text>Feedback</Text>
+        <Button onPress={() => {
+          console.log("Hello world");
+        }}>
+          <ButtonText>
+            Hello world
+          </ButtonText>
+        </Button>
       </Box>
     </SwipeableComponent>
   );
