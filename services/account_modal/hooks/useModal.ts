879c6af3e9669e475be1c1558974ec576d26afa4
Anthony Yolach Lloveras <innoendo.itsolutions@gmail.com>
2025-10-14 09:28:58 +0800

    commit latest account modal implementation

--- services/account_modal/hooks/useModal.ts
+++ services/account_modal/hooks/useModal.ts
@@ -9,7 +9,6 @@ export const useModal = () => {
     selectedAccount,
     setSelectedAccount,
     accounts,
-    setAccounts,
   } = useContext(AccountModalContext);
 
   const _signOut = async () => {
