b07f567a4c4a15afbd272a8e28b995082b479dc7
Anthony Yolach Lloveras <innoendo.itsolutions@gmail.com>
2025-10-15 14:58:13 +0800

    refactored loading screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,9 @@
 import "@/global.css";
 
-import { GluestackUIProvider, ModeType } from "@/components/ui/gluestack-ui-provider";
+import {
+  GluestackUIProvider,
+  ModeType,
+} from "@/components/ui/gluestack-ui-provider";
 import ThemedLoaderScreen from "@/components/LoadingIndicator/ThemedLoadingScreen";
 
 import { useEffect, useState, createContext, useContext, useRef } from "react";
@@ -10,7 +13,6 @@ import * as SplashScreen from "expo-splash-screen";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import "react-native-reanimated";
-
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -51,13 +53,12 @@ const ColorModeContext = createContext<ColorModeContextType | undefined>(
 // Hook to use the color mode context
 export const useColorMode = () => {
   const context = useContext(ColorModeContext);
-  if (context === undefined) throw new Error("useColorMode must be used within a ColorModeProvider");
+  if (context === undefined)
+    throw new Error("useColorMode must be used within a ColorModeProvider");
   return context;
 };
 
-export const MockAccountSwitch = () => {
-
-}
+export const MockAccountSwitch = () => {};
 
 const DEFAULT_COLOR_MODE = "light";
 
@@ -70,28 +71,46 @@ function RootLayoutNav() {
   const [isCompleted, setIsCompleted] = useState(false);
   const mockAccount = {
     name: "Raccoons FC",
-    avatar: "https://plus.unsplash.com/premium_photo-1723600867732-a925c995c888?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHNxdWFyZSUyMHBvcnRyYWl0fGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=900",
+    avatar:
+      "https://plus.unsplash.com/premium_photo-1723600867732-a925c995c888?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHNxdWFyZSUyMHBvcnRyYWl0fGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=900",
     theme: "Pivot Culinary & Gameday",
-    alias: "RFC"
-  }
+    alias: "RFC",
+  };
   useEffect(() => {
     if (prevColorMode.current === colorMode) return;
     prevColorMode.current = colorMode;
+    setIsCompleted(false); // Triggers Fade out of the themed loader screen
     setIsSwitchingApp(true); // Triggers Fade in of the themed loader screen
     setTimeout(() => {
       setIsCompleted(true); // Triggers display of the current theme
-    }, 3000);
+    }, 2000);
     setTimeout(() => {
-      setIsSwitchingApp(false); // Triggers Fade out of the themed loader screen
+      setIsSwitchingApp(false);
     }, 4000);
   }, [colorMode]);
+
+  useEffect(() => {
+    console.log(
+      "isCompleted",
+      isCompleted,
+      " isSwitchingApp",
+      isSwitchingApp,
+      "colorMode",
+      colorMode
+    );
+  }, [isCompleted, isSwitchingApp, colorMode]);
   //** IMPLEMENTATION FOR THEMED LOADER SCREEN */
   return (
     <ColorModeContext.Provider value={{ colorMode, setColorMode }}>
       <GestureHandlerRootView className="flex-1">
         <GluestackUIProvider mode={colorMode}>
           {/** //* THEMED LOADER SCREEN */}
-          <ThemedLoaderScreen theme={colorMode} switching={isSwitchingApp} completed={isCompleted} account={mockAccount}>
+          <ThemedLoaderScreen
+            theme={colorMode}
+            switching={isSwitchingApp}
+            completed={isCompleted}
+            account={mockAccount}
+          >
             <Stack>
               <Stack.Screen name="(auth)" options={{ headerShown: false }} />
               <Stack.Screen
@@ -100,7 +119,7 @@ function RootLayoutNav() {
               />
               <Stack.Screen name="landing" options={{ headerShown: false }} />
             </Stack>
-          </ThemedLoaderScreen>          
+          </ThemedLoaderScreen>
         </GluestackUIProvider>
       </GestureHandlerRootView>
     </ColorModeContext.Provider>
