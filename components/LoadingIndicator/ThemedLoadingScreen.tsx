5373ca465be1ef653844f7de7106619df729e595
Anthony Yolach Lloveras <innoendo.itsolutions@gmail.com>
2025-10-16 08:50:17 +0800

    clean up code.

--- components/LoadingIndicator/ThemedLoadingScreen.tsx
+++ components/LoadingIndicator/ThemedLoadingScreen.tsx
@@ -6,31 +6,18 @@ import { HStack } from "@/components/ui/hstack";
 import { VStack } from "@/components/ui/vstack";
 import { Text } from "@/components/ui/text";
 
-import Animated, {
-  FadeIn,
-  FadeInDown,
-  FadeOut,
-  FadeOutUp,
-  interpolateColor,
-  useAnimatedStyle,
-  useSharedValue,
-  withDelay,
-  withTiming,
-} from "react-native-reanimated";
-import { useEffect, useState } from "react";
+import Animated, { FadeIn, FadeOut, FadeOutUp } from "react-native-reanimated";
 import { StyleSheet } from "react-native";
 import { cssInterop } from "nativewind";
 import { Image } from "expo-image";
-import { Avatar, AvatarFallbackText, AvatarImage } from "../ui/avatar";
+import {
+  Avatar,
+  AvatarFallbackText,
+  AvatarImage,
+} from "@/components/ui/avatar";
 import AppLogo from "@/components/AppLogo";
 
-const THEME_LOADER_SCREEN_FADE_DURATION = 250;
-const THEME_LOADER_SCREEN_START_FADE_DELAY = 50;
-const THEME_LOADER_SCREEN_FADE_DELAY = 1000;
-const THEME_TRANSITION_DURATION = 1000;
 const LOADING_INDICATOR_SIZE = 64;
-const GAMEDAY_BLUE = "#022D55";
-const PIVOT_BLUE = "#212945";
 
 cssInterop(Image, { className: "style" });
 
@@ -39,47 +26,6 @@ const getOtherTheme = (theme: ModeType) => {
 };
 
 export default function ThemedLoaderScreen(props: ThemedLoaderScreenProps) {
-  const appearance = useSharedValue(0);
-  const [showTransition, setShowTransition] = useState(false);
-  const themeTransition = useSharedValue(0);
-  //   const containerViewAnimatedStyle = useAnimatedStyle(
-  //     () => ({
-  //       opacity: appearance.value,
-  //       backgroundColor: interpolateColor(
-  //         themeTransition.value,
-  //         [0, 1],
-  //         props.theme === "light"
-  //           ? [PIVOT_BLUE, GAMEDAY_BLUE]
-  //           : [GAMEDAY_BLUE, PIVOT_BLUE]
-  //       ),
-  //     }),
-  //     [props.theme]
-  //   );
-
-  //   useEffect(() => {
-  //     cancelAnimationFrame(appearance.value);
-  //     if (props.switching) {
-  //       appearance.value = withDelay(
-  //         THEME_LOADER_SCREEN_START_FADE_DELAY,
-  //         withTiming(1, { duration: THEME_TRANSITION_DURATION })
-  //       );
-  //     } else if (props.completed) {
-  //       cancelAnimationFrame(themeTransition.value);
-  //       setShowTransition(true);
-  //       themeTransition.value = withTiming(1, {
-  //         duration: THEME_TRANSITION_DURATION,
-  //       });
-  //       appearance.value = withDelay(
-  //         THEME_TRANSITION_DURATION,
-  //         withTiming(0, { duration: 1000 })
-  //       );
-  //     }
-  //     return () => {
-  //       themeTransition.value = 0;
-  //       setShowTransition(false);
-  //     };
-  //   }, [props.switching, props.completed]);
-
   return (
     <>
       {props.children}
