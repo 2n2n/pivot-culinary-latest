be01b690dfa082a1689318d9cc0834cd9f1d3c5e
3 <www.reinhart@gmail.com>
2025-11-21 16:43:28 +0800

    * added contact type to TripleseatEvent

--- types.d.ts
+++ types.d.ts
@@ -193,6 +193,7 @@ declare global {
     deleted_at?: string | null;
     location: Location;
     booking: Booking;
+    contact: Contact;
     custom_fields: CustomField[];
     documents?: TripleseatDocument[];
   };
